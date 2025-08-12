95d1d9f3a23037bcf3e5ecd191f806a305836ab6
Cholade <151749907+Cholade@users.noreply.github.com>
2025-08-12 12:22:16 +0100

    Add files via upload

---
+++ repo.py
@@ -0,0 +1,10 @@
+my_list = []
+my_list.append[10,20,30,40]
+print(my_list)
+my_list.insert(2,15)
+print(my_list)
+del my_list(-1)
+print(my_list)
+my_list.sort()
+print(my_list)
+print(my_list.index[3])
